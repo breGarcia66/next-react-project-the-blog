edcad139d138b3ba2709b057f79f479c82997dbd
Brenno Levy <brennol3vy@gmail.com>
2026-05-02 00:17:01 -0300

    testando server actions para revalidar cache do next

---
+++ src/db/drizzle/tests.ts
@@ -0,0 +1,9 @@
+import { eq } from "drizzle-orm";
+import { drizzleDb } from ".";
+import { postsTable } from "./schemas";
+
+(async () => {
+  await drizzleDb.update(postsTable).set({
+    title: 'Rotina matinal de pessoas altamente eficazes'
+  }).where(eq(postsTable.slug, 'rotina-matinal-de-pessoas-altamente-eficazes'))
+})()
