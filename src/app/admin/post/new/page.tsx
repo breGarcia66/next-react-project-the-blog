5954dc4a4184e84c6c9ed7828149d7a537f8df87
Brenno Levy <brennol3vy@gmail.com>
2026-05-27 14:44:49 -0300

    criando componenet InputText

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -5,8 +5,14 @@ import { BugIcon } from 'lucide-react';
 export default async function AdminNewPost() {
   return (
     <div className='py-16 flex flex-col gap-4'>
-      <InputText type='text' placeholder='Digite seu nome' />
-      <InputText type='text' placeholder='Digite seu sobrenome' />
+      <InputText labelText='nome' type='text' placeholder='Digite seu nome' />
+      <InputText labelText='sobrenome' type='text' placeholder='Digite seu sobrenome' />
+
+      <InputText disabled labelText='nome' type='text' placeholder='Digite seu nome' />
+      <InputText disabled labelText='sobrenome' type='text' placeholder='Digite seu sobrenome' />
+
+      <InputText readOnly labelText='nome' type='text' defaultValue='Digite seu nome' />
+      <InputText readOnly labelText='sobrenome' type='text' defaultValue='Digite seu sobrenome' />
     </div>
   );
 }
--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -1,9 +1,48 @@
-type InputTextProps = {} & React.ComponentProps<'input'>;
+import clsx from 'clsx';
+import { useId } from 'react';
 
-export function InputText({ ...props }: InputTextProps) {
+type InputTextProps = {
+  labelText?: string;
+} & React.ComponentProps<'input'>;
+
+export function InputText({ labelText = '', ...props }: InputTextProps) {
+  const inputId = useId();
+
+  const inputTextStyles = clsx(
+    'outline-0',
+    'ring-0',
+    'ring-stone-400',
+    'bg-stone-200',
+    'text-base/tight',
+    'ring-1',
+    'rounded-xs',
+    'p-1',
+    'transition',
+    'placeholder-stone-400',
+    'focus:ring-stone-600',
+
+    'disabled:bg-stone-300',
+    'disabled:placeholder-stone-400',
+
+    'read-only:bg-stone-200',
+    'read-only:ring-transparent',
+    'read-only:text-stone-400',
+
+    props.className,
+  );
+
   return (
-    <>
-      <input {...props} />
-    </>
+    <div className='flex flex-col gap-2'>
+      {labelText && (
+        <label htmlFor={inputId} className='text-sm'>
+          {labelText}
+        </label>
+      )}
+      <input
+        id={inputId}
+        {...props}
+        className={inputTextStyles}
+       />
+    </div>
   );
 }
